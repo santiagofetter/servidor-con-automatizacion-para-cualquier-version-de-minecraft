9a6d241bb7f1bef17fbba30f9e507d5cca0dc80d
santiagofetter <santiago.fetter.martinez2012@gmail.com>
2025-06-23 16:19:51 -0300

    Add files via upload
    automatizacion del servidor

---
+++ startm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Configuración
+MINECRAFT_JAR="server.jar" #colocas el nombre del archivo del servidor
+JAVA_MEMORY="12G" # colocas la ram que quieras ponerle al servidor en este caso esta en 12G pero es recomendable 4G para jugar de entre 2 y 5 jugadores.
+
+# Verificar si el archivo JAR existe
+if [ ! -f "$MINECRAFT_JAR" ]; then
+    echo "Error: No se encontró $MINECRAFT_JAR. ¿Estás en el directorio correcto?"
+    exit 1
+fi
+
+# Iniciar Minecraft (en primer plano para ver logs en terminal)
+echo "servidor de minecraft iniciado. ahora has en otra terminal: ./startp.sh"
+java -Xmx"$JAVA_MEMORY" -jar "$MINECRAFT_JAR" nogui
+echo "servidor de minecraft cerrado, espero que les alla gustado"
