f34c7bc209f00c57ebe490aab8943b1cad3e76f2
santiagofettercopy <segundaria.de.santiago@gmail.com>
2025-06-25 17:10:45 -0600

    Create anti_afk.sh

---
+++ anti_afk.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# anti afk para que no se cierre. 
+
+echo "Anti-AFK activado. Presiona Ctrl+C para detener."
+
+while true; do
+
+  echo -n "" # Envía una línea vacía sin salto de línea
+  sleep 60 # Espera 1 minuto (60 segundos) puedes cambiarlo a tu placer.
+done
