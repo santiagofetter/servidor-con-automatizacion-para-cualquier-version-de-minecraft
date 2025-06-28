cac31e97d83c189733a660299c4395a4252b1085
santiagofetter <santiago.fetter.martinez2012@gmail.com>
2025-06-27 19:04:36 -0600

    Update startp.sh

--- startp.sh
+++ startp.sh
@@ -1,22 +1,43 @@
 #!/bin/bash
+echo "  ╔══════════════════════════════════════════════════════╗"
+echo "  ║      ██████╗ ██╗      █████╗ ██╗   ██╗██╗████████╗   ║"
+echo "  ║      ██╔══██╗██║     ██╔══██╗╚██╗ ██╔╝██║╚══██╔══╝   ║"
+echo "  ║      ██████╔╝██║     ███████║ ╚████╔╝ ██║   ██║      ║"
+echo "  ║      ██╔═══╝ ██║     ██╔══██║  ╚██╔╝  ██║   ██║      ║"
+echo "  ║      ██║     ███████╗██║  ██║   ██║   ██║   ██║      ║"
+echo "  ║      ╚═╝     ╚══════╝╚═╝  ╚═╝   ╚═╝   ╚═╝   ╚═╝      ║"
+echo "  ║                          .gg                         ║"
+echo "  ╠══════════════════════════════════════════════════════╣"
 
 # Iniciar Playit en segundo plano y capturar la IP
+chmod +x playit-linux-amd64
 nohup ./playit-linux-amd64 > playit.log 2>&1 &
 
-# Esperar 5 segundos para que Playit genere la IP sin errores
+# Esperar 5 segundos para que Playit genere la IP
 sleep 5
 
-# Extraer la IP del archivo de log y mostrarla
-PLAYIT_IP=$(grep -o "https://[a-zA-Z0-9.-]*\.gl\.joinmc\.link" playit.log | head -n 1)
+# Extraer la IP del log
+PLAYIT_IP=$(grep -a -o "[a-zA-Z0-9.-]*.gl.joinmc.link" playit.log | head -n 1)
+
+echo ""
+echo "╔═════════════════════════════════════════════════╗"
+echo "║             ¡Servicio a través de Playit.gg!    ║"
+echo "╠═════════════════════════════════════════════════╣"
 
 if [ -z "$PLAYIT_IP" ]; then
-    echo "Revisa playit.log para ver la ip"
+    echo "║ ⚠️  No pudimos obtener la dirección de Playit.gg. ║"
+    echo "║    Por favor, revisa el archivo playit.log.     ║"
 else
-    echo ""
-    echo "--------------------------------------------------"
-    echo "¡Servidor listo! Conéctate usando:"
-    echo "IP Pública:  $PLAYIT_IP"
-    echo "IP Local:    127.0.0.1:25565"
-    echo "--------------------------------------------------"
-    echo ""
+    echo "║ ✅ playit marchando sin problema                ║"
+    echo "║                                                 ║"
+    echo "║ Para que TÚ y tus amigos se conecten:           ║"
+    echo "║                                                 ║"
+    echo "║ 🌐  DIRECCIÓN DE CONEXIÓN:                      ║"
+    echo "║     -->  $PLAYIT_IP                             ║"
+    echo "║                                                 ║"
+    echo "║                 Playit.gg                       ║"
+    echo "║                                                 ║"
+    echo "║                                                 ║"
 fi
+echo     "╚═════════════════════════════════════════════════╝"
+echo ""
